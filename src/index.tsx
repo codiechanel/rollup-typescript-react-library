735c067e23071c86b682c3b1800db4e20abc5838
codie <codiechanel@gmail.com>
2019-10-25 20:17:53 +0800

    added jest

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,18 @@
-import * as React from "react";
+import * as React from 'react'
 
-export { default as Greatness } from "./Greatness";
-export { default as Best } from "./Best";
+// export { default as Greatness } from "./Greatness";
+// export { default as Best } from "./Best";
 
-export default function Cool() {
-  return <div>Hello hi</div>;
+// export default function Cool() {
+//   return <div>Hello hi</div>;
+// }
+
+export type Props = { text: string }
+
+export default class ExampleComponent extends React.Component<Props> {
+  render() {
+    const { text } = this.props
+
+    return <div style={{ color: 'red' }}>Hello {text}</div>
+  }
 }
